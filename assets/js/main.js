58591b688add860a90e50327d5b54b635f2ed5f3
Harsh Rana <harshrana117@gmail.com>
2021-08-15 14:43:49 +0530

    Navbar up and down on clicking done

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,13 @@ if(navToggle){
     navMenu.classList.add('show_menu')
   })
 }
+
+/*======== Remove Menu Mobile =======*/
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+  const navMenu = document.getElementById('nav_menu');
+  navMenu.classList.remove('show_menu');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
