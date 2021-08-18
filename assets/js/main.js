4c5f35d907364f87270f68c5510b37961ee7e75e
Harsh Rana <harshrana117@gmail.com>
2021-08-18 09:24:02 +0530

    Portfolio Swiper done

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,6 +60,21 @@ tabs.forEach(tab => {
   })
 })
 
+/*=============== Swiper Js ================*/
+
+let swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
 
 
 
