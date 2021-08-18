e84365d9a00a5c6b645b1af2fdcc85d1654974d6
Harsh Rana <harshrana117@gmail.com>
2021-08-18 09:44:40 +0530

    Portfolio Swiper arrow fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,17 +62,17 @@ tabs.forEach(tab => {
 
 /*=============== Swiper Js ================*/
 
-let swiper = new Swiper(".mySwiper", {
+let swiper = new Swiper(".portfolio_container", {
   cssMode: true,
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
-  mousewheel: true,
-  keyboard: true,
 });
 
 
